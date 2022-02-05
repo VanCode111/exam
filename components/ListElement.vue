0b8c043f09bbaca0787041b986edafe2c7688e0b
VanCode111 <71648558+VanCode111@users.noreply.github.com>
2022-02-05 20:58:56 +0300

    fix

--- components/ListElement.vue
+++ components/ListElement.vue
@@ -52,9 +52,6 @@
       <v-card-text class="text--primary">
         <div>Площадь: {{ square }}</div>
       </v-card-text>
-      <v-card-text class="text--primary">
-        <div>Объем двигателя: {{ engine_volume }}</div>
-      </v-card-text>
 
       <v-card-actions>
         <v-btn color="orange" text> Открыть</v-btn>
